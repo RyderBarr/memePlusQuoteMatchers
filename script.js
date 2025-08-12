754523bc03260c9b1d57a37538f699a4557b005e
ryder <tootwo2to22@gmail.com>
2025-08-12 15:26:34 -0700

    push

---
+++ script.js
@@ -0,0 +1,31 @@
+let memes = [
+    'https://i.pinimg.com/236x/23/41/7c/23417c1ee1e24c31446b5666d5017595.jpg',
+    'https://images.cdn-files-a.com/uploads/1918596/2000_5c676ebc029b5.jpg',
+    'https://www.meme-arsenal.com/memes/d29321fec73e3036daa58d08482527d8.jpg',
+    'https://pbs.twimg.com/profile_images/1467777507675951115/Nl3EgI_y_400x400.jpg',
+    'https://content.imageresizer.com/images/memes/Screaming-meme-6.jpg',
+    'https://www.meme-arsenal.com/memes/70fc4ad29ec9e77759738a4526d31952.jpg',
+    'https://static.pbslearningmedia.org/media/collections/1aguu4.jpg',
+    'https://pm1.aminoapps.com/7571/98cdfbd20c23039902e3a46025498e77d518e094r1-749-442v2_hq.jpg',
+    'https://i.pinimg.com/200x150/1d/d9/10/1dd9102e9e36e28f26adddfca9fddb21.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu7QYxOTZ6LHEc8Dep6irpbDuy35_QTVGQTA&s',
+    'https://i.pinimg.com/236x/50/fe/2b/50fe2b0cd9274aca7575556e1b6644d9.jpg',
+    'https://i.imgur.com/4NdtQF2.png',
+    'https://ih1.redbubble.net/image.4725745836.5629/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg'
+]
+
+let quotes = [
+    'when there is only 5min left of lunch',
+    'when you want to hang up but can\'t because your freind is yapping',
+    'when your parents won\'t get the skylander that cost $10 dollars',
+    'I mean you know',
+    '*confused screaming*',
+    'me: tomorrow is my friend\'s birthday also me, the day after:',
+    'POV: \" do you like my new hair cut\"',
+    'when someone doesn\'t just use an attacking move',
+    'Is this a pigeon',
+    'POV: kanye',
+    'Hey kid I have a PS4 down here',
+    'If you lay down in snow, would you technically be swimming?',
+    'loss.jpg'
+]
