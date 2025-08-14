0c6149e1df5e9a85b537360a18f618494bc83d36
ryder <tootwo2to22@gmail.com>
2025-08-14 15:25:50 -0700

    somuchdone

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 let memes = [
-    'https://i.pinimg.com/236x/23/41/7c/23417c1ee1e24c31446b5666d5017595.jpg',// 1
-    'https://images.cdn-files-a.com/uploads/1918596/2000_5c676ebc029b5.jpg',// 2
-    'https://www.meme-arsenal.com/memes/d29321fec73e3036daa58d08482527d8.jpg',//3
-    'https://ih1.redbubble.net/image.4725745836.5629/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg',//4
-    'https://content.imageresizer.com/images/memes/Screaming-meme-6.jpg',// 5
-    'https://www.meme-arsenal.com/memes/70fc4ad29ec9e77759738a4526d31952.jpg',// 6
-    'https://static.pbslearningmedia.org/media/collections/1aguu4.jpg',// 7
-    'https://pm1.aminoapps.com/7571/98cdfbd20c23039902e3a46025498e77d518e094r1-749-442v2_hq.jpg',// 8
-    'https://i.pinimg.com/200x150/1d/d9/10/1dd9102e9e36e28f26adddfca9fddb21.jpg',// 9
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu7QYxOTZ6LHEc8Dep6irpbDuy35_QTVGQTA&s', //10
-    'https://i.pinimg.com/236x/50/fe/2b/50fe2b0cd9274aca7575556e1b6644d9.jpg',// 11
-    'https://i.redd.it/z10g17uoptx91.jpg',// 12
-    'https://www.treehugger.com/thmb/_Laad-vOB6g7JbXo15sSfZY9B8I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2017__04__Platypus-Swimming-Water-61eea67f3ffa47d18bf47e79d73f2c6e.jpg',//13
-    'https://i.ytimg.com/vi/yV_wA62bbtk/maxresdefault.jpg',//14
-    'https://www.vexforum.com/uploads/default/original/3X/6/4/64a5140c67b1b3552ab6e890e535c2fb0c3ee8e9.jpeg',//15
-    'https://i1.sndcdn.com/avatars-0Zvzb8BbIDCteRnH-X0P3Ng-t1080x1080.jpg',//16
-    'https://ih1.redbubble.net/image.2527780669.6340/raf,360x360,075,t,fafafa:ca443f4786.jpg',//17
-    'https://i.imgflip.com/8tw3vb.png',//18
-    'https://i.imgflip.com/27qxmb.jpg',//19
-    'https://pbs.twimg.com/profile_images/1467777507675951115/Nl3EgI_y_400x400.jpg',//20
-
+    ['https://i.pinimg.com/236x/23/41/7c/23417c1ee1e24c31446b5666d5017595.jpg','sonic the glizzy goblin'],
+    ['https://images.cdn-files-a.com/uploads/1918596/2000_5c676ebc029b5.jpg','old stock photo guy'],
+    ['https://www.meme-arsenal.com/memes/d29321fec73e3036daa58d08482527d8.jpg','sad cat'],
+    ['https://ih1.redbubble.net/image.4725745836.5629/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg','loss.png'],
+    ['https://content.imageresizer.com/images/memes/Screaming-meme-6.jpg','filthy franke screaming'],
+    ['https://www.meme-arsenal.com/memes/70fc4ad29ec9e77759738a4526d31952.jpg','side eye'],
+    ['https://static.pbslearningmedia.org/media/collections/1aguu4.jpg', 'sloth with bad hair line'],
+    ['https://pm1.aminoapps.com/7571/98cdfbd20c23039902e3a46025498e77d518e094r1-749-442v2_hq.jpg', 'suprized pickachu'],
+    ['https://i.pinimg.com/200x150/1d/d9/10/1dd9102e9e36e28f26adddfca9fddb21.jpg', 'is this a bird?'],
+    ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu7QYxOTZ6LHEc8Dep6irpbDuy35_QTVGQTA&s', 'the wall Pink Floyd'],
+    ['https://i.pinimg.com/236x/50/fe/2b/50fe2b0cd9274aca7575556e1b6644d9.jpg','it'],
+    ['https://i.redd.it/z10g17uoptx91.jpg','cat in the hat'],
+    ['https://www.treehugger.com/thmb/_Laad-vOB6g7JbXo15sSfZY9B8I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2017__04__Platypus-Swimming-Water-61eea67f3ffa47d18bf47e79d73f2c6e.jpg','platypus swimming in water'],
+    ['https://i.ytimg.com/vi/yV_wA62bbtk/maxresdefault.jpg', 'I don\' remember this guy\'s name'],
+    ['https://www.vexforum.com/uploads/default/original/3X/6/4/64a5140c67b1b3552ab6e890e535c2fb0c3ee8e9.jpeg', 'gru'],
+    ['https://i1.sndcdn.com/avatars-0Zvzb8BbIDCteRnH-X0P3Ng-t1080x1080.jpg', 'the bee movie'],
+    ['https://ih1.redbubble.net/image.2527780669.6340/raf,360x360,075,t,fafafa:ca443f4786.jpg','four wierd cats'],
+    ['https://i.imgflip.com/8tw3vb.png','bell curve'],
+    ['https://i.imgflip.com/27qxmb.jpg', 'im something of a '],
+    ['https://i1.sndcdn.com/artworks-s5htOUZ86I68UneP-gss7pQ-t500x500.jpg', 'time to stop'],
+    ['https://imgflip.com/s/meme/Laughing-Leo.png','leo '],
+    ['https://i.ytimg.com/vi/XR9uwISDnRk/maxresdefault.jpg','obama'],
+    ['https://i.imgflip.com/29s5ao.jpg','elmo'],
+    ['https://i.imgflip.com/2tdam9.png','let me in'],
+    ['https://i.imgflip.com/1i40lv.jpg','sisysphusis'],
+    ['https://i.imgflip.com/8gmiw7.jpg','sad hampster'],
+    ['https://i.imgflip.com/52m3ru.png','paul allen?'],
+    ['https://media.tenor.com/GYN8AgO9O2cAAAAM/godzilla-slide.gif', 'go go godzilla slide kick'],
+    ['https://pbs.twimg.com/profile_images/1467777507675951115/Nl3EgI_y_400x400.jpg',' you know the rules and so do i']
 ]
 
-let quotes = [
+quotes = [
     'when there is only 5min left of lunch',//1
     'when you want to hang up but can\'t because your freind is yapping',//2
     'when your parents won\'t get the skylander that cost $10 dollars',//3
-    'loss.jpg',//4
+    'loss.png',//4
     '*confused screaming*',//5
     'me: tomorrow is my friend\'s birthday <br> also me, the day after:',//6
     'POV: <br> \" do you like my new hair cut\"',//7
@@ -42,6 +50,28 @@ let quotes = [
     'the zoom meeting just started',//17
     'whale are fish | whale are mammals | whale are fish',//18
     'you know im something of a spider-man my self',//19
+    'it\'s time to stop',//20
+    'when a 20 minute Pink Floyd song starts playing',//21
+    'obama pyramid',//22
+    'me in the repo that doesn\'t make any sense to any one but me',//23
+    'When I get to class 40 minutes early',//
+    'hard coding',
+    'the way im looking at the officer when \'I didn\'t mean to hit the family of four\'',
+    'very good <br> let\'s see Paul Allen\'s PC set up',
+    `when i go go godzilla slide kick
+    <div class="flex flex-row justify-center items-between"> 
+    <img src="https://media.tenor.com/GYN8AgO9O2cAAAAM/godzilla-slide.gif" alt="" class='w-[2vw] p-2'>
+    <img src="https://media.tenor.com/GYN8AgO9O2cAAAAM/godzilla-slide.gif" alt="" class='w-[4vw] p-2'>
+    <img src="https://media.tenor.com/GYN8AgO9O2cAAAAM/godzilla-slide.gif" alt="" class='w-[6vw] p-2'>
+    <img src="https://media.tenor.com/GYN8AgO9O2cAAAAM/godzilla-slide.gif" alt="" class='w-[8vw] p-2'>
+    <img src="https://media.tenor.com/GYN8AgO9O2cAAAAM/godzilla-slide.gif" alt="" class='w-[10vw] p-2'>
+    <img src="https://media.tenor.com/GYN8AgO9O2cAAAAM/godzilla-slide.gif" alt="" class='w-[10vw] p-2'>
+    <img src="https://media.tenor.com/GYN8AgO9O2cAAAAM/godzilla-slide.gif" alt="" class='w-[8vw] p-2'>
+    <img src="https://media.tenor.com/GYN8AgO9O2cAAAAM/godzilla-slide.gif" alt="" class='w-[6vw] p-2'>
+    <img src="https://media.tenor.com/GYN8AgO9O2cAAAAM/godzilla-slide.gif" alt="" class='w-[4vw] p-2'>
+    <img src="https://media.tenor.com/GYN8AgO9O2cAAAAM/godzilla-slide.gif" alt="" class='w-[2vw] p-2'>
+    </div>
+    `,
     `I mean you know <br> the rules <br> <span class=" text-xs ">
     A full commitment's what I'm thinkin' of <br>
     You wouldn't get this from any other guy <br>
@@ -82,7 +112,7 @@ let quotes = [
     Never gonna give you up, never gonna let you down <br>
     Never gonna run around and desert you <br>
     Never gonna make you cry, never gonna say goodbye <br>
-    Never gonna tell a lie and hurt you </span>`,//20
+    Never gonna tell a lie and hurt you </span>`,//24
 ]
 
 let index = [
@@ -105,7 +135,7 @@ $(document).ready(function(){
             $('#gallery').append(`
                 
                 <div class='flex flex-col m-5 justify-center items-center border bg-yellow-300'>
-                    <img src="${memes[i]}" alt="" class='w-[70vw] p-2'>
+                    <img src="${memes[i][0]}" alt="${memes[i][1]}" class='w-[70vw] p-2'>
                     <h2 class=' p-2 w-[70vw] text-center self-center text-purple-500 font-bold text-2xl'>${quotes[i]}</h2>
                 </div>
 
@@ -128,28 +158,29 @@ $(document).ready(function(){
 
     })
 
-    $('#suffle').on('click', function(event)
+    $('#shuffle').on('click', function(event)
     {
 
-        randnum = Math.floor(Math.random() * 20 + 1)
+        randnum = Math.floor(Math.random() * (memes.length-2) + 2)
+        
         $('#gallery').html('')
 
-        for(let i = randnum; i == randnum-1;i++)
+        for(let i = randnum; i != randnum+1;i--)
         {
 
             $('#gallery').append(`
                 
                 <div class='flex flex-col m-5 justify-center items-center border bg-yellow-300'>
-                    <img src="${memes[i]}" alt="" class='w-[70vw] p-2'>
+                    <img src="${memes[i][0]}" alt="${memes[i][1]}" class='w-[70vw] p-2'>
                     <h2 class=' p-2 w-[70vw] text-center self-center text-purple-500 font-bold text-2xl'>${quotes[i]}</h2>
                 </div>
 
                 `)
 
-            if(i == 20)
+            if(i == 0)
             {
 
-                i = 0;
+                i = memes.length;
 
             }
 
@@ -167,34 +198,43 @@ $(document).ready(function(){
     $('#randomCombo').on('click', function(event)
     {
 
+        $('#gallery').html('')
+
+        for (let i = 0;i < memes.length; i++)
+        {
+
+            let meme = i + Math.floor(Math.random() * 100);
+
+            while(meme > memes.length)
+            {
+
+                meme=meme-memes.length
+
+            }
+
+            let quote = i + Math.floor(Math.random() * 100);
+
+            while(quote > memes.length)
+            {
+
+            
+                quote=quote-memes.length
+
+            }
+    
+            $('#gallery').append(`
+                
+                <div class='flex flex-col m-5 justify-center items-center border bg-yellow-300 opacity-85'>
+                    <img src="${memes[meme][0]}" alt="${memes[meme][1]}" class='w-[70vw] p-2' opacity-75>
+                    <h2 class=' p-2 w-[70vw] text-center self-center text-purple-500 font-bold text-2xl opacity-85'>${quotes[quote]}</h2>
+                </div>
+
+                `)
+
+        }
+
         
 
     })
     
 })
-
-
-
-
-
-function addContent(url, quote)
-{
-
-    memes.push(url);
-    quotes.push(quote);
-
-}
-
-function shuffleContent()
-{
-
-    populateGallery(true)
-
-}
-
-function randomCombo()
-{
-
-    
-
-}
